47747d32cbb7de7104608f69fde10f7a6b6fbb89
Tran Son Tung <tungts1101@gmail.com>
2021-09-27 08:28:28 +0700

    Update structure of PointUNet

--- src/pointunet.py
+++ src/pointunet.py
@@ -11,73 +11,221 @@ class UnpoolObj(nn.Module):
         super(UnpoolObj, self).__init__()
     
     def forward(self, xyz, points):
-        return random_walk_torch(xyz.permute(0, 2, 1), 64, 0.05), random_walk_torch(points.permute(0, 2, 1), 64, 0.0)
+        return random_walk_torch(xyz.squeeze(-1).permute(0, 2, 1), 32, 0.05), random_walk_torch(points.squeeze(-1).permute(0, 2, 1), 32, 0.0)
 
 class PoolObj(nn.Module):
     def __init__(self):
         super(PoolObj, self).__init__()
     
     def forward(self, xyz, points):
-        _xyz = xyz.permute(0, 2, 1)
-        _points = points.permute(0, 2, 1)
+        # print(xyz.shape)
+        # print(points.shape)
+
+        # _xyz = xyz.permute(0, 2, 1)
+        # _points = points.permute(0, 2, 1)
+        _xyz = xyz.squeeze(-1).permute(0, 2, 1)
+        _points = points.squeeze(-1).permute(0, 2, 1)
+
+        # print(_xyz.shape)
 
         B, N, C = _xyz.shape
         idx = farthest_point_sample(_xyz, N//2)
         return index_points(_xyz, idx), index_points(_points, idx)
 
+nstates_plus_1 = [64,64,128]
+nstates_plus_2 = [128,128,256]
+nstates_plus_3 = [256,512,1024,1024,512]
 class PointUNetObj(nn.Module):
-    def __init__(self):
+    def __init__(self, ball_radius2):
         super(PointUNetObj, self).__init__()
-        self.sa1 = PointNetSetAbstraction(npoint=512, radius=0.04, nsample=64, mlp=[64, 64, 128], in_channel=6, group_all=False) # 512x32
-        self.sa5 = PointNetSetAbstraction(npoint=128, radius=0.1, nsample=64, mlp=[128, 128, 256], in_channel=128+3, group_all=False) # 128x32
+        # self.sa1 = PointNetSetAbstraction(npoint=512, radius=0.04, nsample=64, mlp=[64, 64, 128], in_channel=6, group_all=False) # 512x32
+        # # self.sa5 = PointNetSetAbstraction(npoint=128, radius=0.1, nsample=64, mlp=[128, 128, 256], in_channel=128+3, group_all=False) # 128x32
+        # self.pool = PoolObj() # 256x128
+        # self.sa2 = PointNetSetAbstraction(npoint=32, radius=0.1, nsample=64, mlp=[128, 128, 256], in_channel=128+3, group_all=False) # 32x32
+        # self.unpool = UnpoolObj() # 896x32
+        # # self.sa3 = PointNetSetAbstraction(npoint=512, radius=0.2, nsample=64, mlp=[256, 256, 512], in_channel=256+3, group_all=False)
+        # # self.sa4 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, mlp=[512, 512, 1024], in_channel=512+3, group_all=True)
+        # self.sa4 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, mlp=[256, 256, 512], in_channel=256+3, group_all=True)
+        # # self.fc1 = nn.Linear(1024, 512)
+        # # self.bn1 = nn.BatchNorm1d(512)
+        # # self.drop1 = nn.Dropout(0.4)
+        # self.fc2 = nn.Linear(512, 256)
+        # self.bn2 = nn.BatchNorm1d(256)
+        # self.drop2 = nn.Dropout(0.4)
+        # self.fc3 = nn.Linear(256, 42)
+
+        self.num_outputs = 42
+        self.knn_K = 64
+        self.ball_radius2 = ball_radius2
+        self.sample_num_level1 = 512
+        self.sample_num_level2 = 32
+        self.INPUT_FEATURE_NUM = 6
+        
+        self.netR_1 = nn.Sequential(
+            # B*INPUT_FEATURE_NUM*sample_num_level1*knn_K
+            nn.Conv2d(self.INPUT_FEATURE_NUM, nstates_plus_1[0], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_1[0]),
+            nn.ReLU(inplace=True),
+            # B*64*sample_num_level1*knn_K
+            nn.Conv2d(nstates_plus_1[0], nstates_plus_1[1], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_1[1]),
+            nn.ReLU(inplace=True),
+            # B*64*sample_num_level1*knn_K
+            nn.Conv2d(nstates_plus_1[1], nstates_plus_1[2], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_1[2]),
+            nn.ReLU(inplace=True),
+            # B*128*sample_num_level1*knn_K
+            nn.MaxPool2d((1,self.knn_K),stride=1)
+            # B*128*sample_num_level1*1
+        )
+        
+        self.netR_2 = nn.Sequential(
+            # B*131*sample_num_level2*knn_K
+            nn.Conv2d(3+nstates_plus_1[2], nstates_plus_2[0], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_2[0]),
+            nn.ReLU(inplace=True),
+            # B*128*sample_num_level2*knn_K
+            nn.Conv2d(nstates_plus_2[0], nstates_plus_2[1], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_2[1]),
+            nn.ReLU(inplace=True),
+            # B*128*sample_num_level2*knn_K
+            nn.Conv2d(nstates_plus_2[1], nstates_plus_2[2], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_2[2]),
+            nn.ReLU(inplace=True),
+            # B*256*sample_num_level2*knn_K
+            nn.MaxPool2d((1,self.knn_K),stride=1)
+            # B*256*sample_num_level2*1
+        )
+        
+        self.netR_3 = nn.Sequential(
+            # B*259*sample_num_level2*1
+            nn.Conv2d(3+nstates_plus_2[2], nstates_plus_3[0], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_3[0]),
+            nn.ReLU(inplace=True),
+            # B*256*sample_num_level2*1
+            nn.Conv2d(nstates_plus_3[0], nstates_plus_3[1], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_3[1]),
+            nn.ReLU(inplace=True),
+            # B*512*sample_num_level2*1
+            nn.Conv2d(nstates_plus_3[1], nstates_plus_3[2], kernel_size=(1, 1)),
+            nn.BatchNorm2d(nstates_plus_3[2]),
+            nn.ReLU(inplace=True),
+            # B*1024*sample_num_level2*1
+            nn.MaxPool2d((1152,1),stride=1),
+            # B*1024*1*1
+        )
+        
+        self.netR_FC = nn.Sequential(
+            # B*1024
+            nn.Linear(nstates_plus_3[2], nstates_plus_3[3]),
+            nn.BatchNorm1d(nstates_plus_3[3]),
+            nn.ReLU(inplace=True),
+            # B*1024
+            nn.Linear(nstates_plus_3[3], nstates_plus_3[4]),
+            nn.BatchNorm1d(nstates_plus_3[4]),
+            nn.ReLU(inplace=True),
+            # B*512
+            nn.Linear(nstates_plus_3[4], self.num_outputs),
+            # B*num_outputs
+        )
+
         self.pool = PoolObj() # 256x128
-        self.sa2 = PointNetSetAbstraction(npoint=32, radius=0.1, nsample=64, mlp=[128, 128, 256], in_channel=128+3, group_all=False) # 32x32
-        self.unpool = UnpoolObj() # 896x32
-        self.sa3 = PointNetSetAbstraction(npoint=512, radius=0.2, nsample=64, mlp=[256, 256, 512], in_channel=256+3, group_all=False)
-        self.sa4 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, mlp=[512, 512, 1024], in_channel=512+3, group_all=True)
-        self.fc1 = nn.Linear(1024, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.drop1 = nn.Dropout(0.4)
-        self.fc2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.drop2 = nn.Dropout(0.4)
-        self.fc3 = nn.Linear(256, 42)
+        self.unpool = UnpoolObj()
     
-    def forward(self, xyz):
-        B, _, _ = xyz.shape
+    # def forward(self, xyz):
+    #     B, _, _ = xyz.shape
 
-        l0_xyz = xyz[:, :, :3]
-        l0_points = xyz[:, :, 3:]
+    #     l0_xyz = xyz[:, :, :3]
+    #     l0_points = xyz[:, :, 3:]
 
-        # print(l0_xyz[0].cpu().numpy()[:10])
-        # pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(l0_xyz[0].cpu().numpy()))
-        # o3d.visualization.draw_geometries([pcd])
+    #     # print(l0_xyz[0].cpu().numpy()[:10])
+    #     # pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(l0_xyz[0].cpu().numpy()))
+    #     # o3d.visualization.draw_geometries([pcd])
 
-        l1_xyz, l1_points = self.sa1(l0_xyz.permute(0, 2, 1), l0_points.permute(0, 2, 1))
-        l5_xyz, l5_points = self.sa5(l1_xyz, l1_points)
+    #     l1_xyz, l1_points = self.sa1(l0_xyz.permute(0, 2, 1), l0_points.permute(0, 2, 1))
+    #     l5_xyz, l5_points = self.sa5(l1_xyz, l1_points)
 
-        l2_xyz_inp, l2_points_inp = self.pool(l1_xyz, l1_points)
-        l2_xyz, l2_points = self.sa2(l2_xyz_inp.permute(0, 2, 1), l2_points_inp.permute(0, 2, 1))
+    #     l2_xyz_inp, l2_points_inp = self.pool(l1_xyz, l1_points)
+    #     l2_xyz, l2_points = self.sa2(l2_xyz_inp.permute(0, 2, 1), l2_points_inp.permute(0, 2, 1))
 
-        l3_xyz_inp, l3_points_inp = self.unpool(l2_xyz, l2_points)
+    #     l3_xyz_inp, l3_points_inp = self.unpool(l2_xyz, l2_points)
 
-        # print(l3_points_inp.shape)
-        # print(l3_points_inp[0][:10])
+    #     # print(l3_points_inp.shape)
+    #     # print(l3_points_inp[0][:10])
 
-        l3_xyz_inp = torch.cat((l5_xyz, l3_xyz_inp.permute(0, 2, 1)), dim=2)
-        l3_points_inp = torch.cat((l5_points, l3_points_inp.permute(0, 2, 1)), dim=2)
-        l3_xyz, l3_points = self.sa3(l3_xyz_inp, l3_points_inp)
+    #     l3_xyz_inp = torch.cat((l5_xyz, l3_xyz_inp.permute(0, 2, 1)), dim=2)
+    #     l3_points_inp = torch.cat((l5_points, l3_points_inp.permute(0, 2, 1)), dim=2)
+    #     # l3_xyz, l3_points = self.sa3(l3_xyz_inp, l3_points_inp)
 
-        # print(l3_xyz.permute(0, 2, 1)[0].cpu().numpy()[:10])
-        # pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(l3_xyz.permute(0, 2, 1)[0].cpu().numpy()))
-        # o3d.visualization.draw_geometries([pcd])
+    #     # print(l3_xyz.permute(0, 2, 1)[0].cpu().numpy()[:10])
+    #     # pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(l3_xyz.permute(0, 2, 1)[0].cpu().numpy()))
+    #     # o3d.visualization.draw_geometries([pcd])
 
-        l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
-        # l4_xyz, l4_points = self.sa4(l3_xyz_inp, l3_points_inp)
-        out = l4_points.view(B, 1024)
+    #     # l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
+    #     # out = l4_points.view(B, 1024)
 
-        out = self.drop1(F.relu(self.bn1(self.fc1(out))))
-        out = self.drop2(F.relu(self.bn2(self.fc2(out))))
-        out = self.fc3(out)
+    #     l4_xyz, l4_points = self.sa4(l3_xyz_inp, l3_points_inp)
+    #     out = l4_points.view(B, 512)
 
-        return out
+    #     # out = self.drop1(F.relu(self.bn1(self.fc1(out))))
+    #     out = self.drop2(F.relu(self.bn2(self.fc2(out))))
+    #     out = self.fc3(out)
+
+    #     return out
+    def forward(self, x, y):
+        B, _, _, _ = x.shape
+
+        # x: B*INPUT_FEATURE_NUM*sample_num_level1*knn_K, y: B*3*sample_num_level1*1
+        x = self.netR_1(x)
+        # B*128*sample_num_level1*1
+        x_ = torch.cat((y, x),1).squeeze(-1)
+        # B*(3+128)*sample_num_level1
+
+        inputs_level2_, inputs_level2_center_ = group_points_2(x_, self.sample_num_level1, 128, self.knn_K, self.ball_radius2)
+        x_ = self.netR_2(inputs_level2_)
+
+        y_pool, x_pool = self.pool(y, x)
+        # print(x_pool.shape, y_pool.shape)
+        x = torch.cat((y_pool.permute(0, 2, 1), x_pool.permute(0, 2, 1)),1)
+
+        # print(x.shape)
+        
+        inputs_level2, inputs_level2_center = group_points_2(x, self.sample_num_level1 // 2, self.sample_num_level2, self.knn_K, self.ball_radius2)
+        # B*131*sample_num_level2*knn_K, B*3*sample_num_level2*1
+
+        # print(inputs_level2.shape, inputs_level2_center.shape)
+        
+        # B*131*sample_num_level2*knn_K
+        x = self.netR_2(inputs_level2)
+        # B*256*sample_num_level2*1
+        # x = torch.cat((inputs_level2_center, x),1)
+        # B*259*sample_num_level2*1
+
+        # print(x.shape)
+
+        y_unpool, x_unpool = self.unpool(inputs_level2_center, x)
+        # print(y_unpool.shape, x_unpool.shape)
+
+        x = x_pool.permute(0, 2, 1)
+
+        x = torch.cat((y_unpool.permute(0, 2, 1), x_unpool.permute(0, 2, 1)), 1).unsqueeze(3)
+
+
+        x_ = torch.cat((inputs_level2_center_, x_), 1)
+        x = torch.cat((x, x_), 2)
+
+        x = self.netR_3(x)
+
+        # print(x.shape)
+
+        # B*1024*1*1
+        
+        x = x.view(-1,nstates_plus_3[2])
+
+        # print(x.shape)
+        # B*1024
+
+        x = self.netR_FC(x)
+        # B*num_outputs
+
+        return x
