b1843457ebc8c29cb015ace9daf6f27fa3f84726
Tran Son Tung <tungts1101@gmail.com>
2021-11-13 22:08:46 +0700

    Update loss function

--- src/train.py
+++ src/train.py
@@ -196,10 +196,14 @@ for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
             if isinstance(network, CascadedNetworkObj):
                 loss = 0.25 * criterion(estimation_stage_1, gt_pca) + 0.25 * criterion(estimation_stage_2, gt_pca) + 0.5 * criterion(estimation, gt_pca)
             else:
+                obb_len = torch.diff(bound_obb, dim=1)
                 if args.contain_obj:
-                    loss = criterion(estimation, torch.cat((gt_xyz, obj_xyz.reshape(-1, 24)), dim=1)) * 1000
+                    # loss = criterion(estimation, torch.cat((gt_xyz, obj_xyz.reshape(-1, 24)), dim=1)) * 1000
+                    loss = 0.9 * criterion(estimation[:, :63].reshape(-1, 21, 3) * obb_len, gt_xyz.reshape(-1, 21, 3) * obb_len) + \
+                        0.1 * criterion(estimation[:, 63:].reshape(-1, 8, 3) * obb_len, obj_xyz.reshape(-1, 8, 3) * obb_len)
                 else:
-                    loss = criterion(estimation, gt_xyz) * 1000
+                    # loss = criterion(estimation, gt_xyz) * 1000
+                    loss = criterion(estimation[:, :63].reshape(-1, 21, 3) * obb_len, gt_xyz.reshape(-1, 21, 3) * obb_len)
 
                 # obb_len = torch.diff(bound_obb, dim=1)
                 # loss = 0.9 * criterion(estimation[:, :63].reshape(-1, 21, 3) * obb_len, gt_xyz.reshape(-1, 21, 3) * obb_len) + \
@@ -275,10 +279,14 @@ for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
                     if isinstance(network, CascadedNetworkObj):
                         eval_loss = 0.25 * criterion(estimation_stage_1, gt_pca) + 0.25 * criterion(estimation_stage_2, gt_pca) + 0.5 * criterion(estimation, gt_pca)
                     else:
+                        obb_len = torch.diff(bound_obb, dim=1)
                         if args.contain_obj:
-                            eval_loss = criterion(estimation, torch.cat((gt_xyz, obj_xyz.reshape(-1, 24)), dim=1)) * 1000
+                            # eval_loss = criterion(estimation, torch.cat((gt_xyz, obj_xyz.reshape(-1, 24)), dim=1)) * 1000
+                            eval_loss = 0.9 * criterion(estimation[:, :63].reshape(-1, 21, 3) * obb_len, gt_xyz.reshape(-1, 21, 3) * obb_len) + \
+                                0.1 * criterion(estimation[:, 63:].reshape(-1, 8, 3) * obb_len, obj_xyz.reshape(-1, 8, 3) * obb_len)
                         else:
-                            eval_loss = criterion(estimation, gt_xyz) * 1000
+                            # eval_loss = criterion(estimation, gt_xyz) * 1000
+                            eval_loss = criterion(estimation[:, :63].reshape(-1, 21, 3) * obb_len, gt_xyz.reshape(-1, 21, 3) * obb_len)
 
                         # obb_len = torch.diff(bound_obb, dim=1)
                         # eval_loss = 0.9 * criterion(estimation[:, :63].reshape(-1, 21, 3) * obb_len, gt_xyz.reshape(-1, 21, 3) * obb_len) + \
